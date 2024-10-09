9f86898be9b9de21528972f1041cd030712a1376
Emanuel Pic <110857302+manupic@users.noreply.github.com>
2024-10-09 20:23:16 -0300

    Add files via upload

--- lab4.sql
+++ lab4.sql
@@ -5,4 +5,25 @@ VALUES ('Acuña 52', 'UTN FRRA Rafaela', 'frra@gmail.com');
 
 /* Ej 2*/
 INSERT INTO Guia(nombre)
-VALUES()
+VALUES('Jorge Perez');
+/* Ej 3*/
+INSERT INTO Escuela(domicilio, nombre)
+SELECT domicilio, nombre
+FROM Escuela
+WHERE idEscuela = 1;
+
+/* Ej 4 */
+DELETE FROM Telefono_Escuela;
+
+INSERT INTO Telefono_Escuela(codigoArea, nro, Escuela_idEscuela)
+VALUES ('1111', '1111',SELECT idEscuela FROM Escuela);
+
+/* Ej 5 */
+UPDATE Telefono_Escuela SET nro = '5445-3223' WHERE idEscuela = 1;
+
+/* Ej 6 */
+UPDATE Reserva SET dia='23-12-2018' WHERE idReserva = 1;
+
+/* Ej 7 */
+
+
