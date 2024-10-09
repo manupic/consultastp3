b951bbf86e43b042dfe95933e03730809edb0195
Emanuel Pic <110857302+manupic@users.noreply.github.com>
2024-10-09 20:31:51 -0300

    Add files via upload

--- lab4.sql
+++ lab4.sql
@@ -25,5 +25,22 @@ UPDATE Telefono_Escuela SET nro = '5445-3223' WHERE idEscuela = 1;
 UPDATE Reserva SET dia='23-12-2018' WHERE idReserva = 1;
 
 /* Ej 7 */
+UPDATE Reserva_Tipo_Visita SET arancelPorAlumno = arancelPorAlumno - 2 WHERE cantidadAlumnosReservados >= 10;
+
+/* Ej 8 */
+
+UPDATE Reserva_Tipo_Visita SET Guia_idGuia = 2 WHERE Guia_idGuia = 1;
+
+/* Ej 9 */
+
+DELETE FROM Reserva_Tipo_Visita WHERE cantidadAlumnosReservados < 10;
+
+/* Ej 10 */
+DELETE FROM Guia WHERE nombre IS NULL;
+
+/* Ej 11 */
+SELECT idGuia, nombre
+FROM Guia
+WHERE nombre LIKE %'Bernardo'%;
 
 
